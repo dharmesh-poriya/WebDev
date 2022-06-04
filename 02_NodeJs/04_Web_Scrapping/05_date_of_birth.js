f7e60872d9ecc68c43d173f003600e42200c0dd4
D.Poriya <dharmeshkporiya@gmail.com>
2022-06-04 08:27:20 +0530

    Last task completed!!

--- 02_NodeJs/04_Web_Scrapping/05_date_of_birth.js
+++ 02_NodeJs/04_Web_Scrapping/05_date_of_birth.js
@@ -15,6 +15,14 @@ function callBack(error,response,html){
     }
 }
 console.log("After requesting");
+let Batter = {
+    name : "",
+    dob : "",
+    display(){
+        console.log(chalk.yellow(this.name)," => ",chalk.green(this.dob));
+    }
+}
+
 function extractHtml(html){
     let $ = cheerio.load(html);
     let winnigTeamArr = $('.ci-team-score.ds-flex.ds-justify-between.ds-items-center.ds-text-typo-title.ds-mb-2 span a span')
@@ -26,44 +34,26 @@ function extractHtml(html){
 
     let innigsArr = $('div>.ds-bg-fill-content-prime.ds-rounded-lg>.ds-mb-4');
     for(let i=0;i<innigsArr.length;i++){
-        // let teamName = $(innigsArr[i]).find('.ds-grow .ds-text-tight-s.ds-font-bold').text();
-        // teamName = teamName.split('INNINGS')[0];
-        // teamName = teamName.trim();
-        // console.log(teamName);
+        let battingArr = $(innigsArr[i]).find('.ReactCollapse--collapse>.ReactCollapse--content>table>tbody')[0];
 
-        // if(teamName === loserTeamName){
-            let battingArr = $(innigsArr[i]).find('.ReactCollapse--collapse>.ReactCollapse--content>table>tbody')[0];
-            // let bowlingArr = $(innigsArr[i]).find('.ReactCollapse--collapse>.ReactCollapse--content>table')[1];
-
-            let battersArr = $(battingArr).find('tr');
-            console.log("length : ",battersArr.length);
-            for(let j=0;j<battersArr.length;j++){
-                let allColsData = $(battersArr[j]).find('td>span>a>span>span');
-                // console.log("allColsData : ",allColsData.length);
-                if(1==allColsData.length){
-                    let Batter = dateOfBirth($(battersArr[j]).find('td')[0],$);
-                }
+        let battersArr = $(battingArr).find('tr');
+        console.log("length : ",battersArr.length);
+        for(let j=0;j<battersArr.length;j++){
+            let allColsData = $(battersArr[j]).find('td>span>a>span>span');
+            if(1==allColsData.length){
+                dateOfBirth($(battersArr[j]).find('td')[0],$);
             }
-            
-        // }
+        }
     }
     
 }
 function dateOfBirth(batter,$){
-    let Batter = {
-        name : "",
-        dob : "",
-        display(){
-            console.log(chalk.green("Name")," : ",chalk.yellow(this.name)," => ",chalk.blue(this.dob));
-        }
-    }
-    let batterName = $(batter).find('span>a>span>span').text();
-    console.log("Batsman : ",batterName);
+    // let batterName = $(batter).find('span>a>span>span').text();
     let profileUrl = 'https://www.espncricinfo.com'+$(batter).find('span>a').attr('href');
     profileUrl.trim();
     // console.log("Profile Url : ",profileUrl);
     request(profileUrl,callBackProfile);
-    return Batter;
+    
 }
 function callBackProfile(error,response,html){
     if(error){
@@ -74,8 +64,10 @@ function callBackProfile(error,response,html){
 }
 function extractHtmlProfile(html){
     let $ = cheerio.load(html);
-    let detailsArr = $('.ds-text-title-xs.ds-font-bold.ds-text-ui-typo>h5');
-    console.log("detailsArr : ",detailsArr.length);
-    let dob = $(detailsArr[14]).find('h5').text();
-    console.log("DOB : ",dob);
+    let detailsArr = $('.ds-p-4>div>div>div>span>h5');
+    Batter.name = $(detailsArr[0]).text();
+    let dob = $(detailsArr[1]).text();
+    // console.log("DOB : ",dob);
+    Batter.dob = dob;
+    Batter.display();
 }
