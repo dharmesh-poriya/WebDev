9d56ff662c136026ca72d8b938bded85435bb716
D.Poriya <dharmeshkporiya@gmail.com>
2022-06-05 16:22:14 +0530

    complete up to finding innings and teams name

--- 02_NodeJs/04_Web_Scrapping/02_Scrapping_IPL/scorecard.js
+++ 02_NodeJs/04_Web_Scrapping/02_Scrapping_IPL/scorecard.js
@@ -17,9 +17,40 @@ function callBack(error,response,html){
 
 // get home page url
 function extractMatchDetailsHtml(html){
+/*
+*venue date opponent result runs balls fours sixes sr
+*• folder structure
+*IPL
+*   team
+*      player
+*         runs balls fours sixes sr opponent venue date result
+*
+*venue date and result is same for both team player.
+*/
+
     let $ = cheerio.load(html);
-    let mainPageLinks = $('.ds-p-0>.ds-block>.ds-py-3.ds-px-4>span>a').attr('href');
-    // console.log("Main page link :- ",mainPageLinks);
-    let fullLink = 'https://www.espncricinfo.com' + mainPageLinks;
-    getAllMatchLinks(fullLink);
+    let descElement = $('.ds-text-tight-m.ds-font-regular.ds-text-ui-typo-mid').text();
+    let result = $('p.ds-text-tight-m.ds-font-regular.ds-truncate.ds-text-typo-title').text();
+
+    let descArr = descElement.split(',');
+    let matchNo = descArr[0].split(' ')[0].trim().slice(0,-2);
+    let dayOrNight = descArr[0].split(' ')[2].trim();
+    let venue = descArr[1].trim();
+    let date = descArr[2].trim();
+    console.log(matchNo,dayOrNight,venue,date,result);
+// venue date => .ds-text-tight-m.ds-font-regular.ds-text-ui-typo-mid
+// result => 
+
+    // innings wise score
+    let innings = $('.ds-mt-3>div>div.ds-bg-fill-content-prime.ds-rounded-lg>.ds-mb-4');
+    // console.log("Teams :- ",innings.length);\
+    for(let i=0;i<innings.length;i++){
+        // finding team name
+        let teamName = $(innings[i]).find('.ds-flex.ds-items-center.ds-cursor-pointer.ds-px-4>.ds-grow>.ds-py-3>span').text();
+        teamName = teamName.split('INNINGS')[0].trim();
+        let opponentIndex = 0==i?1:0;
+        let opponentTeamName = $(innings[opponentIndex]).find('.ds-flex.ds-items-center.ds-cursor-pointer.ds-px-4>.ds-grow>.ds-py-3>span').text();
+        opponentTeamName = opponentTeamName.split('INNINGS')[0].trim();
+        // console.log(chalk.yellow(teamName),chalk.green('vs'),chalk.red(opponentTeamName));
+    }
 }
