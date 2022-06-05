3224106d72c2aa597db0c79099d67019c657cb28
D.Poriya <dharmeshkporiya@gmail.com>
2022-06-05 19:01:14 +0530

    batsman and bowler details done

--- 02_NodeJs/04_Web_Scrapping/02_Scrapping_IPL/main.js
+++ 02_NodeJs/04_Web_Scrapping/02_Scrapping_IPL/main.js
@@ -1,6 +1,7 @@
 import request from 'request';
 import cheerio from 'cheerio';
 import chalk from 'chalk';
+import {getAllMatchLinks} from './all_matches.js';
 
 const url = 'https://www.espncricinfo.com/series/ipl-2021-1249214';
 
@@ -24,22 +25,3 @@ function extractHtml(html){
     getAllMatchLinks(fullLink);
 }
 
-// get all match links
-function getAllMatchLinks(fullLink){
-    request(fullLink,function (error,response,html){
-        if(error) console.error(chalk.bgRedBright(error));
-        else extractMatchLinks(html);
-    });
-}
-
-function extractMatchLinks(html){
-    let $ = cheerio.load(html);
-    let scoreBoardElement = $('.ds-p-0>.ds-flex.ds-flex-wrap>.ds-border-b.ds-border-line>div>.ds-px-4.ds-py-3');
-    let scoreBoardLinks = [];
-    
-    for(let i=0;i<scoreBoardElement.length;i++){
-        // console.log("A scoreboard links :- ",$(scoreBoardElement[i]).find('a').attr('href'));
-        scoreBoardLinks.push('https://www.espncricinfo.com'+$(scoreBoardElement[i]).find('a').attr('href'));
-    }
-    // console.log("Scoreboard links :- ",scoreBoardLinks);
-}
--- 02_NodeJs/04_Web_Scrapping/02_Scrapping_IPL/scorecard.js
+++ 02_NodeJs/04_Web_Scrapping/02_Scrapping_IPL/scorecard.js
@@ -52,5 +52,50 @@ function extractMatchDetailsHtml(html){
         let opponentTeamName = $(innings[opponentIndex]).find('.ds-flex.ds-items-center.ds-cursor-pointer.ds-px-4>.ds-grow>.ds-py-3>span').text();
         opponentTeamName = opponentTeamName.split('INNINGS')[0].trim();
         // console.log(chalk.yellow(teamName),chalk.green('vs'),chalk.red(opponentTeamName));
+
+        // finding batting and bowling innings
+        let oneInnings = $(innings[i]).find('.ReactCollapse--collapse>.ReactCollapse--content>table>tbody');
+        let currentBatting = $(oneInnings[0]).find('tr.ds-border-b.ds-border-line.ds-text-tight-s');
+        let currentBowling = $(oneInnings[1]).find('tr.ds-border-b.ds-border-line.ds-text-tight-s');
+        
+        // for current batting innings each valid row contain 8 column 
+        for(let j=0;j<currentBatting.length;j++){
+            let allCols = $(currentBatting[j]).find('td');
+            // console.log('Length',allCols.length);
+            let isValid = (8===allCols.length)?true:false;
+            if(true===isValid){
+                let batterName = $(allCols[0]).find('span>a>span>span').text().trim();
+                let states = $(allCols[1]).find('span>span').text().trim();
+                let runs = $(allCols[2]).text().trim();
+                let balls = $(allCols[3]).text().trim();
+                // let matchesPlayed = $(allCols[4]).text().trim(); // not sure about M
+                let fours = $(allCols[5]).text().trim();
+                let sixes = $(allCols[6]).text().trim();
+                let sr = $(allCols[7]).text().trim();
+                console.log(chalk.yellow(batterName),chalk.green(states),chalk.red(runs),chalk.blue(balls),chalk.magenta(fours),chalk.cyan(sixes),chalk.green(sr));
+            }
+        }
+        console.log('-----------------------------------------------');
+        // for current bowling innings each valid row contain 11 column
+        for(let j=0;j<currentBowling.length;j++){
+            let allCols = $(currentBowling[j]).find('td');
+            // console.log('Length',allCols.length);
+            let isValid = (11===allCols.length)?true:false;
+            if(true===isValid){
+                let bowlerName = $(allCols[0]).find('span>a>span').text();
+                let overs = $(allCols[1]).text();
+                let maiden = $(allCols[2]).text();
+                let runs = $(allCols[3]).text();
+                let wickets = $(allCols[4]).find('strong').text();
+                let economy = $(allCols[5]).text();
+                let zeros = $(allCols[6]).text();
+                let fours = $(allCols[7]).text();
+                let sixes = $(allCols[8]).text();
+                let wide = $(allCols[9]).text();
+                let noBall = $(allCols[10]).text();
+                console.log(chalk.yellow(bowlerName),chalk.green(overs),chalk.red(maiden),chalk.blue(runs),chalk.magenta(wickets),chalk.cyan(economy),chalk.green(zeros),chalk.red(fours),chalk.blue(sixes),chalk.magenta(wide),chalk.cyan(noBall));
+            }
+        }
+        console.log('-----------------------------------------------');
     }
 }
